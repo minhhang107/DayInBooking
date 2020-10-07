ccbcb06c86a2c8039a280ac75059fe3011187439
Minh Hang Nguyen <hang00103@gmail.com>
2020-10-06 22:30:16 -0400

    add room-details + change search-results

--- app.js
+++ app.js
@@ -13,8 +13,20 @@ app.get('/index', function(req, res){
     res.sendFile(path.join(__dirname,"/views/index.html"))
 });
 
-app.get('/room-listing', function(req, res){
-    res.sendFile(path.join(__dirname,"/views/room-listing.html"))
+app.get('/search-result', function(req, res){
+    res.sendFile(path.join(__dirname,"/views/search-result.html"))
+});
+
+app.get('/room-details', function(req, res){
+    res.sendFile(path.join(__dirname,"/views/room-details.html"))
+});
+
+app.get('/book-now', function(req, res){
+    res.sendFile(path.join(__dirname,"/views/book-now.html"))
+});
+
+app.get('/upload-room', function(req, res){
+    res.sendFile(path.join(__dirname,"/views/upload-room.html"))
 });
 
 
