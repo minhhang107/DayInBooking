27c0a26db02f01e1ee94d136fbc1ec541bbdb668
Minh Hang Nguyen <hang00103@gmail.com>
2020-12-04 21:32:03 -0500

    add .env

--- app.js
+++ app.js
@@ -2,21 +2,16 @@ var express = require('express');
 var app= express();
 require("dotenv").config();
 var bodyParser = require('body-parser')
-var multer = require("multer");
 const hbs = require('express-handlebars');
 const clientSessions = require("client-sessions");
 var mongoose = require("mongoose");
-var Schema = mongoose.Schema;
-var userController = require('./controllers/userController');
-var adminController = require('./controllers/adminController');
-var userModel = require("./models/userModel");
-var roomModel = require("./models/roomModel")
+
 
 const userRoutes = require("./controllers/userController");
 const generalRoutes = require("./controllers/generalController");
 const adminRoutes = require("./controllers/adminController");
 
-mongoose.createConnection("mongodb://localhost/DayIn", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.createConnection(process.env.DBCONN, { useNewUrlParser: true, useUnifiedTopology: true });
 
 var HTTP_PORT = process.env.PORT || 8080;
 
@@ -30,7 +25,7 @@ app.use(clientSessions({
     cookieName: "session",
     secret: "Web322",
     duration: 30*60*1000,
-    activeDuration: 1000*60
+    activeDuration: 5*60*1000
 }))
 
 app.use(bodyParser.json());
--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-module.exports = {
-    dbconn: "mongodb://localhost/DayIn"
-    //dbconn: "mongodb+srv://mhnguyen16:Web3222020@cluster0.oobyl.mongodb.net/DayIn?retryWrites=true&w=majority"
-}
+// module.exports = {
+//     dbconn: "mongodb://localhost/DayIn"
+//     //dbconn: "mongodb+srv://mhnguyen16:Web3222020@cluster0.oobyl.mongodb.net/DayIn?retryWrites=true&w=majority"
+// }
